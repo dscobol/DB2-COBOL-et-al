ced4f503fe6872fc58cc7405e9941f1e9a9f2f57
dastagg:win10 <dastagg@stagglinux.com>
2020-07-30 09:47:32 -0400

    Update compiler option to ibm-strict.

--- cobol/dastagg/sql-test2/compile.sh
+++ cobol/dastagg/sql-test2/compile.sh
@@ -19,4 +19,12 @@ db2cmd -i -w -c db2 -tvf db2_precompile.sql
 # pause
 read -n1 -r -p "Press any key to continue..." key
 
-cobc -x -std=mf $PGM.cbl -I $SYSLIB -L $LOADLIB -l db2api
+# Added ibm-strict compile flag.
+# Note: Because SQLTEST2 removed CHECKERR module, it can
+# be compiled with ibm-strict.
+# Removed these copybooks:
+#           COPY "sqlenv.cbl".
+#           COPY "sql.cbl".
+# which are MicroFocus copybooks that contain a level 78 value.
+# -std=ibm will allow it, -std=ibm-strict will not. 
+cobc -x -std=ibm-strict $PGM.cbl -I $SYSLIB -L $LOADLIB -l db2api
