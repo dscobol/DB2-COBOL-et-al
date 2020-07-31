deec2f46e801db14f784ee6a47e82a4ca4145393
dastagg:win10 <dastagg@stagglinux.com>
2020-07-31 20:49:47 +0100

    Start of SQTEST3

---
+++ cobol/dastagg/sql-test3/db2_precompile.sql
@@ -0,0 +1,4 @@
+connect to SAMPLE user dastagg using password;
+prep SQTEST3.sqb bindfile target ANSI_COBOL;
+bind SQTEST3.bnd;
+connect reset;
---
+++ cobol/dastagg/sql-test3/run.sh
@@ -0,0 +1,8 @@
+# run program
+
+PGM=SQTEST3
+
+export COB_LIBRARY_PATH="$RHOME/IBM/SQLLIB/BIN"
+export COB_PRE_LOAD=db2agapi64
+
+./$PGM.exe
