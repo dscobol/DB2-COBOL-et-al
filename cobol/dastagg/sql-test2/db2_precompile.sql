0430f8ddfb3615891067464ef6f8f9ab97babd9f
dastagg:opus4 <dastagg@stagglinux.com>
2020-08-19 12:06:51 -0400

    Clean up.

--- cobol/dastagg/sql-test2/db2_precompile.sql
+++
@@ -1,4 +0,0 @@
-connect to SAMPLE user dastagg using password;
-prep SQTEST2.sqb bindfile target ANSI_COBOL;
-bind SQTEST2.bnd;
-connect reset;
--- cobol/dastagg/sql-test2/run.sh
+++
@@ -1,8 +0,0 @@
-# run program
-
-PGM=SQTEST2
-
-export COB_LIBRARY_PATH="$RHOME/IBM/SQLLIB/BIN"
-export COB_PRE_LOAD=db2agapi64
-
-./$PGM.exe
